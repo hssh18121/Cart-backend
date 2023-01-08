7d9c2e3130a5ffa947d035e7097ac6bb384d7f9e
nduongw <duong5112001@gmail.com>
2023-01-09 03:33:44 +0700

    add to cart api

--- config/db.php
+++ config/db.php
@@ -19,5 +19,13 @@ class Database {
           
           return $this->conn;
     }
+
+    public function get_username() {
+      return $this->username;
+    }
+
+    public function get_password() {
+      return $this->password;
+    }
 }
 ?>
